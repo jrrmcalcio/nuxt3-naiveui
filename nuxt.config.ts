aff0274c2f22ddf1c788cca081385b6bdc809b93
Julio Rodas <22454285+jrrmcalcio@users.noreply.github.com>
2021-10-22 18:04:29 -0600

    chore: install naive-ui library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 import { defineNuxtConfig } from 'nuxt3'
 
 export default defineNuxtConfig({
+  build: {
+    transpile: ['naive-ui', 'vfonts'],
+  },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
+import { NButton } from 'naive-ui';
 <template>
   <div>
     <h1>Home Page</h1>
+    <n-button>naive-ui</n-button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { NButton } from 'naive-ui'
+</script>
